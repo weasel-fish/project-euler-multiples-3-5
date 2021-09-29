8b51535cdd1f21cdfc782752d7a6f804ed2f675c
github username <github email address>
2021-09-29 10:51:57 -0400

    Done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,29 @@
 # Enter your procedural solution here!
+
+def collect_multiples(num)
+    # array = (1...num).to_a
+    # mults = []
+
+    # array.each do |n|
+    #     if n % 3 === 0 || n % 5 === 0
+    #         mults << n
+    #     end
+    # end
+    
+    # mults
+
+    (1...num).to_a.reject { |n| n % 3 != 0 && n % 5 != 0}
+end
+
+def sum_multiples(num)
+    # array = collect_multiples(num)
+    # i = 0
+    # sum = 0
+    # while i < array.length
+    #     sum += array[i]
+    #     i += 1
+    # end
+    # sum
+
+    collect_multiples(num).inject(0, :+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,18 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_reader :top, :multiples
+
+
+    def initialize(num)
+        @top = num - 1
+        @multiples = collect_multiples
+    end
+    
+    def collect_multiples
+        (1..top).to_a.reject { |n| n % 3 != 0 && n % 5 != 0}
+    end
+
+    def sum_multiples
+        multiples.inject(0, :+)
+    end
+end
